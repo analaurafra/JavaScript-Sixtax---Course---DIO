195160c74b3c58b5f692470514ee1daeda8877bd
analaurafra <92827626+analaurafra@users.noreply.github.com>
2021-11-01 18:25:06 -0300

    Add files via upload

---
+++ Aula 05 - Funcoes/script.js
@@ -0,0 +1,11 @@
+/* function funcao () {
+    console.log('Whats Up Man?!');
+}
+
+funcao(); */
+
+function mensagem (primeiro,segundo) {
+    console.log(primeiro, segundo)
+}
+
+mensagem('Whats Up','Man?!');
