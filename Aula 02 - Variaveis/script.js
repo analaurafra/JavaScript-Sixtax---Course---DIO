9c102191355b32175da4d0b7b1f44acdf4377e30
analaurafra <92827626+analaurafra@users.noreply.github.com>
2021-11-01 18:23:29 -0300

    Add files via upload

---
+++ Aula 02 - Variaveis/script.js
@@ -0,0 +1,118 @@
+//Tipos Primitivos
+
+/* //boleanos (True or False)
+var vouF = false;
+console.log (typeof(vouF));*/
+
+/* //Tipo Number
+var numeroQualquer = 1;
+console.log (typeof(numeroQualquer));*/
+
+/* //string
+var nome = "ana";
+console.log(typeof(nome)); */
+
+/* //A variável é uma caixa que guarda valores
+//Para guardar a variavel temos 3 formas: VAR
+var texto = 'Ana Laura';
+texto = 'Martins';
+console.log(texto); */
+
+/* //let
+let texto02 = "Ana";
+texto02 = "Laura";
+console.log (texto02); */
+
+/* //Const (No teste ao dar F12 deve aparecer uma mensagem de erro)
+const texto03 = "Ana";
+texto03 = "Jesus";
+console.log(texto03);*/
+
+/* //escopos
+var escopoGlobal = 'global';
+console.log(escopoGlobal); 
+function escopoLocal (){
+    let escopoLocalinterno = 'local';
+    console.log(escopoLocalinterno);
+ }
+escopoLocal(); */
+
+/* //atribuicao
+var atribuicao = 'Ana';
+console.log(atribuicao); */
+
+/* //comparacao
+var comparacao = '0' == 0;
+console.log (comparacao); */
+
+/* //comparacao identifica
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica); */
+
+/* //adição
+var adicao = 1+1;
+console.log(adicao); */
+
+/* //subtração
+var subtracao = 2-1;
+console.log(subtracao); */
+
+/* //multiplicação
+var multiplicacao = 3*3;
+console.log(multiplicacao); */
+
+/* //divisão real
+var divisao = 30/6;
+console.log(divisao); */
+
+/* //divisão inteira
+var divisaoInteira = 5;
+console.log(divisaoInteira); */
+
+
+/* //potenciacao
+var potenciacao = 2 ** 10;
+console.log(potenciacao); */
+
+//Operadores Relacionais
+
+/* //Maior Que
+var maiorQue = 5 > 2;
+console.log (maiorQue); */
+
+/* //Menos Que
+var menorQue = 6 < 12;
+console.log (menorQue); */
+
+/* // Maior ou Igual A 
+var maiorOuIgual = 10 >= 20;
+console.log (maiorOuIgual); */
+
+/* // Menor ou Igual A 
+var menorOuIgual = 30 <= 40;
+console.log (menorOuIgual); */
+
+//Operadores Lógicos 
+
+/* // && considera que todos os valores sejam True
+var e = true && false;
+console.log (e); */
+
+/* // // considera que QUALQUER valor seja True
+var ou = true || false;
+console.log(ou); */
+
+/*// ! Inverte o valor de true para false ou vice e versa
+var nao = true ! false;
+console.log(nao); */
+
+
+
+
+
+
+
+
+ 
+
+
